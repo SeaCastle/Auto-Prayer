1a13adcef5668c96ede34f9e754f355d961a565c
Spencer Peterson <spencerthomasp@gmail.com>
2021-05-27 17:19:33 -0600

    Code cleanup

--- auto-prayer.js
+++ auto-prayer.js
@@ -2,7 +2,7 @@
 
 console.log("Loading Auto-Prayer");
 
-// Get the enemy attack string
+// Get the associated nodes
 const enemyAttackDescNode = document.getElementById("combat-enemy-attack-speed-desc");
 const activePrayersNode = document.getElementById("combat-player-active-prayers");
 const protectFromRangedPrayerNode = document.getElementById("combat-prayer-16");
@@ -11,10 +11,10 @@ const attack = "shockwave";
 
 // For some reason the console.log function inside the observable callback is going insane. Make sure it only checks once per second
 let currentTime = Date.now();
-let publishInterval = 1000; // ms
+let publishInterval = 1000; // ms; I don't think any enemies attack faster than 1s so this shouldn't cause toggle lag but idk
 let nextPublishTime = currentTime + publishInterval;
 
-// Callback for handling what to do with attack description
+// Callback for handling what to do with enemy attack
 const checkEnemyAttack = function(mutationsList, observer) {
   
   currentTime = Date.now();
@@ -22,32 +22,29 @@ const checkEnemyAttack = function(mutationsList, observer) {
     if (mutationsList[0].target.innerText.toLowerCase().includes(attack)) {
       console.log(`Enemies attack is now  ${attack}`);
 
-      // Check protectFromRangedPrayerNode does have class d-none before toggling on
+      // Check that prayer is not active before toggling on
       if (protectFromRangedPrayerNode.classList.contains("d-none")) {
         console.log("Toggling prayer on");
         protectFromeRangedPrayerBtn.onclick.apply(protectFromeRangedPrayerBtn);
-        //togglePrayer(16);
       }
     } else {
       console.log("We are not interested in enemy attack");
-      // Check protectFromRangedPrayer does not have class d-none before toggling off
+
+      // Check that prayer is currently active before toggling off
       if (!protectFromRangedPrayerNode.classList.contains("d-none")) {
         console.log("Toggling prayer off");
         protectFromeRangedPrayerBtn.onclick.apply(protectFromeRangedPrayerBtn);
-        //togglePrayer(16);
       }
     }
 
     nextPublishTime = currentTime + publishInterval;
   }
-
 }
 
-const enemyAttackObserver = new MutationObserver(checkEnemyAttack);
-
 // Mutation observer options
-// childList mutation fires on innerHTML change.
+// NOTE: childList mutation fires on innerHTML change which is what we want to observe
 const mutObserverOptions = {childList: true};
+const enemyAttackObserver = new MutationObserver(checkEnemyAttack);
 
 // Wire up the observer
 enemyAttackObserver.observe(enemyAttackDescNode, mutObserverOptions);
@@ -56,4 +53,3 @@ enemyAttackObserver.observe(enemyAttackDescNode, mutObserverOptions);
 // Example strings
 //Using <strong>Elusiveness</strong>:
 //Using <strong>Shockwave</strong>:
-
